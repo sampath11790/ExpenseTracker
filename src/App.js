b14c6d2de4d99f909bec4803901a11e6cbcee44f
sampath11790 <bsampath11790@gmail.com>
2023-01-19 12:08:34 +0530

    added lazy loading

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React, { useEffect } from "react";
+import React, { useEffect, Suspense } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
-import UserPage from "./components/Login/UserPage";
-import AuthForm from "./components/Login/Login";
-import ContactDetails from "./components/Login/ContactDetails";
-import LoadingSpinner from "./components/Loadingspinner/LoadingSpinner";
-import ExpenseForm from "./components/Expense/ExpenseForm";
+// import UserPage from "./components/Login/UserPage";
+// import AuthForm from "./components/Login/Login";
+// import ContactDetails from "./components/Login/ContactDetails";
+// import LoadingSpinner from "./components/Loadingspinner/LoadingSpinner";
+// import ExpenseForm from "./components/Expense/ExpenseForm";
 import { useSelector, useDispatch } from "react-redux";
-import Header from "./components/Header/Header";
+// import Header from "./components/Header/Header";
 import { AuthSliceAction } from "./store/Auth";
 import "./App.css";
+
+const Header = React.lazy(() => import("./components/Header/Header"));
+const ExpenseForm = React.lazy(() =>
+  import("./components/Expense/ExpenseForm")
+);
+const LoadingSpinner = React.lazy(() =>
+  import("./components/Loadingspinner/LoadingSpinner")
+);
+const ContactDetails = React.lazy(() =>
+  import("./components/Login/ContactDetails")
+);
+const AuthForm = React.lazy(() => import("./components/Login/Login"));
+const UserPage = React.lazy(() => import("./components/Login/UserPage"));
 function App() {
   const Dispatch = useDispatch();
 
@@ -24,40 +37,45 @@ function App() {
   // const loginState = localStorage.getItem("islogin") === "true";
   console.log(dark);
   return (
-    <div className={dark || !loginState ? "light" : ""}>
-      {loginState && <Header></Header>}
-      {console.log("app rendeing")}
-      {!loginState ? (
-        <Route path="/login">
-          <AuthForm></AuthForm>
-        </Route>
-      ) : (
-        <Switch>
-          <Route path="/contactdetails">
-            <ContactDetails></ContactDetails>
-          </Route>
+    <>
+      {" "}
+      <Suspense fallback={<p className="loading">Loading .....</p>}>
+        <div className={dark || !loginState ? "light" : ""}>
+          {loginState && <Header></Header>}
+          {console.log("app rendeing")}
+          {!loginState ? (
+            <Route path="/login">
+              <AuthForm></AuthForm>
+            </Route>
+          ) : (
+            <Switch>
+              <Route path="/contactdetails">
+                <ContactDetails></ContactDetails>
+              </Route>
 
-          <Route path="/userpage" exact>
-            <UserPage></UserPage>
-          </Route>
-          <Route path="/loading">
-            <LoadingSpinner></LoadingSpinner>
-          </Route>
-          <Route path="/expenseform" exact>
-            <ExpenseForm></ExpenseForm>
-          </Route>
-          <Route path="/login" exact>
-            <AuthForm></AuthForm>
-          </Route>
+              <Route path="/userpage" exact>
+                <UserPage></UserPage>
+              </Route>
+              <Route path="/loading">
+                <LoadingSpinner></LoadingSpinner>
+              </Route>
+              <Route path="/expenseform" exact>
+                <ExpenseForm></ExpenseForm>
+              </Route>
+              <Route path="/login" exact>
+                <AuthForm></AuthForm>
+              </Route>
 
-          <Route path="*">
-            <Redirect to="/login" />
-          </Route>
-        </Switch>
-      )}
-      {!loginState && <Redirect to="/login" />}
-      {loginState && <Redirect to="/expenseform" />}
-    </div>
+              <Route path="*">
+                <Redirect to="/login" />
+              </Route>
+            </Switch>
+          )}
+          {!loginState && <Redirect to="/login" />}
+          {loginState && <Redirect to="/expenseform" />}
+        </div>
+      </Suspense>
+    </>
   );
 }
 
